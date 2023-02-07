8f5dd8086cee7a3fdeddc42a996f373870c9f3ee
JVandenildo <jvandenildo@gmail.com>
2023-02-07 07:58:55 -0300

    Last Guesses, text updates

--- Vanilla/HSCardGuesser.js
+++ Vanilla/HSCardGuesser.js
@@ -1,33 +1,57 @@
 var card = {
     title: "none",
     type: "none",
-    attack: 6,
-    life: 7,
-    cost: 6
-};
+    attack: "none",
+    life: "none",
+    cost: "none",
 
+    tries: 0
+};
 function cardSpit(){
-    alert(card.cost);
+    resetGame();
+
     let date = new Date();
     let randomLuck = Math.round(Math.random(date) * 10);
-    
-    card.cost = randomLuck;
-    alert(card.cost);
 
-    resetGame();
+    card.title = randomLuck.toString();
 }
 
 const inputGuess = document.querySelector('#inputGuess');
 const buttonGuess = document.querySelector('#buttonGuess');
 buttonGuess.addEventListener("click", luckTester);
 
+var lastGuessesList = [];
+const lastGuesses = document.querySelector('.lastGuesses');
 function luckTester(){
-    alert(card.cost);
+    switch (card.title) {
+        case "none":
+            alert("You need to spit the card first!");
+            
+            break;
+        
+        case inputGuess.value:
+            card.tries++;
+            alert(`Acertou com ${card.tries} tentativas.`);
 
-    resetGame();
+            break;
+    
+        default:
+            card.tries++;
+            lastGuessesList.push(inputGuess.value);
+            lastGuesses.insertAdjacentHTML("beforeend", `<p>${lastGuessesList[lastGuessesList.length - 1]}</p>`);
+            alert(`Your card ${card.title}. Tries: ${card.tries}.`);
+
+            break;
+    }
+
+    inputGuess.value = "";
 }
 
 
 function resetGame(){
-    return inputGuess.value = "";
+    inputGuess.value = "";
+    card.tries = 0;
+    card.title = "none";
+    lastGuessesList.length = 0;
+    lastGuesses.innerHTML = "<h3>Last Guesses</h3>";
 }
