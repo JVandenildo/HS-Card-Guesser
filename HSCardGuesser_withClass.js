0dda98d1b61f62ae87a772c90f7abb06b85c2037
J. Vandenildo <jvandenildo@gmail.com>
2023-06-17 17:22:05 -0300

    object parse

--- HSCardGuesser_withClass.js
+++ HSCardGuesser_withClass.js
@@ -1,6 +1,5 @@
 /* thoughts on the user process:
-click on generate card
-button should:
+click on generate card button, this button should:
 - fetch api;
 - modify all of card attributes?
     - create only 1 class to rule them all?
@@ -11,6 +10,35 @@ const lastGuesses = document.querySelector(".lastGuesses");
 const cardSpitter = document.querySelector("#cardSpitter");
 cardSpitter.addEventListener("click", cardSpit);
 
+class Card {
+	constructor(name, id) {
+		this._name = name;
+		this._id = id;
+	}
+
+	cardFetch() {
+		let apiURL =
+			"https://api.hearthstonejson.com/v1/95431/enUS/cards.collectible.json";
+
+		fetch(apiURL)
+			.then(function (response) {
+				return response.json();
+			})
+			.catch(() =>
+				alert(
+					"API could not be reached at this time. This means your luck won't be tested now.\nSorry, kiddo."
+				)
+			);
+	}
+
+	allInfo() {
+		lastGuesses.innerHTML = `<b>Id:</b> ${this._id};<br>
+        <b>Name:</b> ${this._name}.`;
+	}
+}
+
 function cardSpit() {
-	lastGuesses.innerHTML = `Working`;
+	const SpittedCard = new Card("Leroy", "EX_001");
+
+	SpittedCard.allInfo();
 }
